a73df037a3bd91ef0ce886b0ff7ef47ee9271564
ponganan <joeyubu@gmail.com>
2024-05-25 18:48:27 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'child-route-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'child-route';
+}
